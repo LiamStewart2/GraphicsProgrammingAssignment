6d30cb3901657f2b4875008da08527439846a432
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2025-03-20 10:38:09 +0000

    Updated Vector3f class for with utility functions

--- OpenGLSimulation/Vector3.h
+++ OpenGLSimulation/Vector3.h
@@ -6,6 +6,31 @@ struct Vector3f
 	Vector3f() {x = 0; y = 0; z = 0;}
 
 	float x, y, z;
+
+	float Magnitude()
+	{
+		return (float) sqrt(x*x + y*y + z*z);
+	}
+	void Normalize()
+	{
+		float m = (float) sqrt(x*x + y*y + z*z);
+		if(m <= 0.0001f) m = 1;
+		x /= m;
+		y /= m;
+		z /= m;
+	}
+	static float dot(Vector3f u, Vector3f v)
+	{
+		return (u.x * v.x + u.y * v.y + u.z * v.z);
+	}
+	static Vector3f Cross(Vector3f u, Vector3f v)
+	{
+		return Vector3f(
+			u.y * v.z - u.z * v.y,
+			-u.x * v.z + u.z * v.x,
+			u.x * v.y - u.y * v.x
+		);
+	}
 };
 
 struct Vector3i
