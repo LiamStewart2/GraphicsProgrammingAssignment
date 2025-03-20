161f1cebf172d7edb360d62f70b8625db2e11c83
liam1stewart2@gmail.com <liam1stewart2@gmail.com>
2025-03-20 13:37:42 +0000

    Removed tomfuckery, we no just move the camera around the monkey

--- OpenGLSimulation/Camera.cpp
+++ OpenGLSimulation/Camera.cpp
@@ -8,26 +8,8 @@ void Camera::Update(double etime)
 
 void Camera::handleMovement(double etime)
 {
-	if (Keyboard::GetButtonState('w'))
-	{
-		eye.z -= 0.01f; 
-		center.z -= 0.01f;
-	}
-	if (Keyboard::GetButtonState('s'))
-	{
-		eye.z += 0.01f; 
-		center.z += 0.01f;
-	}
-	if(Keyboard::GetButtonState('a'))
-	{
-		eye.x = sin(etime * 0.001f) * 6;
-		eye.z = cos(etime * 0.001f) * 6;
-	}
-	if (Keyboard::GetButtonState('d'))
-	{
-		eye.x += 0.01f;
-		center.x += 0.01f;
-	}
+	eye.x = sin(etime * 0.001f) * 6;
+	eye.z = cos(etime * 0.001f) * 6;
 }
 
 void Camera::handleTurning()
